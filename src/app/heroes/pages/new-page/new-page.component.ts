8497b93533dcc9de47a12ffa2feaac73953a11ab
anibalUniversales <aemunozh@universales.com>
2024-05-02 16:10:09 -0600

    Nueva página de heroe HTML

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class NewPageComponent {
-
+  public publishers = [
+    { id: 'Marvel Comics', desc: 'Marvel - Comics' },
+    { id: 'DC Comics', desc: 'DC - Comics' },
+  ];
 }
