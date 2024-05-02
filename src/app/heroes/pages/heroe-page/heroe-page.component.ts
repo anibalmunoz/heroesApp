ce4aa2ce900085c81b59bedbf2067f4475fa6b7f
anibalUniversales <aemunozh@universales.com>
2024-05-02 14:25:45 -0600

    Página de heroe

--- src/app/heroes/pages/heroe-page/heroe-page.component.ts
+++ src/app/heroes/pages/heroe-page/heroe-page.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-heroe-page',
   templateUrl: './heroe-page.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class HeroePageComponent {
+export class HeroePageComponent implements OnInit {
+  public hero?: Hero;
 
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+      .pipe(switchMap(({ id }) => this.heroesService.getHeroById(id)))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigate(['/heroes/list']);
+        this.hero = hero;
+        return;
+      });
+  }
+
+  goBack() {
+    this.router.navigateByUrl('heroes/list');
+  }
 }
