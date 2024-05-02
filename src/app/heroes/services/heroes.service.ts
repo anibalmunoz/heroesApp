ad53f5dd7eb389a317cc9aacf309382e641186d0
anibalUniversales <aemunozh@universales.com>
2024-05-02 17:14:59 -0600

    Servicio para crear, actualizar y borrar

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from 'src/environments/environments.prod';
 
@@ -25,6 +25,25 @@ export class HeroesService {
       ///Este url no me funcionó en postman 2 de mayo 2024 (no funciona el q= para buscar nombres de heroes)
       // `${this.baseUrl}/heroes/?q=${query}&limit=6`
       `${this.baseUrl}/heroes?superhero=${query}`
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id es requerido');
+    return this.httpClient.patch<Hero>(
+      `${this.baseUrl}/heroes/${hero.id}`,
+      hero
+    );
+  }
+
+  deleteHero(id: string): Observable<boolean> {
+    return this.httpClient.delete<Hero>(`${this.baseUrl}/heroes/${id}`).pipe(
+      catchError((error) => of(false)),
+      map((resp) => true)
     );
   }
 }
