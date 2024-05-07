2b93927034d98c6950edef336a56c26e93a656ad
anibalUniversales <aemunozh@universales.com>
2024-05-07 15:32:46 -0600

    Crear y editar heroe

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,13 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [],
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
+  public heroForm = new FormGroup({
+    id: new FormControl(''),
+    superhero: new FormControl('', { nonNullable: true }),
+    publisher: new FormControl<Publisher>(Publisher.DCComics),
+    alter_ego: new FormControl(''),
+    first_appearance: new FormControl(''),
+    characters: new FormControl(''),
+    alt_img: new FormControl(''),
+  });
+
   public publishers = [
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
     { id: 'DC Comics', desc: 'DC - Comics' },
   ];
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private snackbar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    if (!this.router.url.includes('edit')) return;
+    this.activatedRoute.params
+      .pipe(switchMap((params) => this.heroesService.getHeroById(params['id'])))
+      .subscribe((hero) => {
+        if (!hero) return this.router.navigateByUrl('/');
+        this.heroForm.reset(hero);
+        return;
+      });
+  }
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+  }
+
+  onSubmit(): void {
+    if (this.heroForm.invalid) return;
+    if (this.currentHero.id) {
+      this.heroesService.updateHero(this.currentHero).subscribe((hero) => {
+        this.showSnackbar(`${hero.superhero}, actualizado`);
+      });
+      return;
+    }
+
+    this.heroesService.addHero(this.currentHero).subscribe((hero) => {
+      this.router.navigate(['/heroes/edit', hero.id]);
+      this.showSnackbar(`${hero.superhero}, creado`);
+    });
+  }
+
+  showSnackbar(message: string) {
+    this.snackbar.open(message, 'done', { duration: 2500 });
+  }
 }
